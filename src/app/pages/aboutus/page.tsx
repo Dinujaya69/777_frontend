ae36ec6933687646f2c7aea5ced3c22029225072
Dinujaya69 <kavinduweerasinha4@gmail.com>
2025-02-21 11:54:45 +0530

    creat abutus

---
+++ src/app/pages/aboutus/page.tsx
@@ -0,0 +1,13 @@
+
+import MainLayout from "@/components/Layouts/MainLayout";
+import React from "react";
+
+const page = () => {
+  return (
+    <MainLayout>
+      <div></div>
+    </MainLayout>
+  );
+};
+
+export default page;
